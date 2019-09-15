b8544f713d1ecc7685dc150f1fae6da9c265cccd
lidagou <843962249@qq.com>
2019-09-15 19:57:50 +0800

    NumberOf1Between1AndN_Solution

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        int[] a = {1,2,3,4,5};
-        int[] b = {3,5,4,2,1};
-        System.out.println(IsPopOrder.IsPopOrder(a,b));
+        System.out.println(NumberOf1Between1AndN_Solution.NumberOf1Between1AndN_Solution(1));
     }
 }
---
+++ src/NumberOf1Between1AndN_Solution.java
@@ -0,0 +1,21 @@
+public class NumberOf1Between1AndN_Solution {
+    public static int NumberOf1Between1AndN_Solution(int n) {
+        int count = 0;
+        /**
+         * 将数字分割成高位与低位
+         * 1234  计算百位1出现次数，分成12高位  34低位，按情况讨论计算如下算法，当百位=0，=1，>=2的不同情况进行计算
+         */
+        for (int i=1;i<=n;i=i*10){
+            int highPlace = n/i;
+            int lowPlace = n - highPlace*i;
+            if (highPlace%10 == 1){
+                count = count + (highPlace/10) * i + lowPlace+1;
+            }else if (highPlace%10 == 0){
+                count = count + (highPlace/10) * i;
+            }else {
+                count = count + ((highPlace/10)+1) * i;
+            }
+        }
+        return count;
+    }
+}
