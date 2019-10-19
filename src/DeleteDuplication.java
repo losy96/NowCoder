03562c50c5b3d68944ccd99c679d0112b2acd050
lidagou <843962249@qq.com>
2019-10-19 18:58:28 +0800

    DeleteDuplication

--- src/DeleteDuplication.java
+++ src/DeleteDuplication.java
@@ -1,4 +1,12 @@
 public class DeleteDuplication {
+    /**
+     * 两种情况：
+     *     若头节点为重复节点，删除重复的节点，选择第一个不重复的节点作为头节点，返回。
+     *     若头节点不重复，将头节点单独拿出，以第二个节点作为头节点，重复上述步骤。
+     *     最后将递归返回节点连接起来，即是答案。
+     * @param pHead
+     * @return
+     */
     public static ListNode deleteDuplication(ListNode pHead) {
         if (pHead == null){
             return null;
