2b16d365582ffe3a4a8c68f0ba35101322923457
lidagou <843962249@qq.com>
2019-11-23 16:14:53 +0800

    minNumberInRotateArray

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,8 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        GetMedian a = new GetMedian();
-        a.Insert(3);
-        a.Insert(6);
-
-        System.out.println(a.GetMedian());
-    }
+        MinNumberInRotateArray a = new MinNumberInRotateArray();
+        int[] i = {1,1,1,1,1,1,0};
+        System.out.println(a.minNumberInRotateArray(i));
+}
 }
--- src/MinNumberInRotateArray.java
+++ src/MinNumberInRotateArray.java
@@ -1,41 +1,62 @@
-//TODO 没通过
+
 public class MinNumberInRotateArray {
-    public static int minNumberInRotateArray(int [] array) {
-        if (array.length==0){
+    /**
+     * 若首尾相等，直接遍历
+     * 若首尾不相等，二分查找
+     * @param array
+     * @return
+     */
+    public int minNumberInRotateArray(int [] array) {
+        if (array.length == 0){
             return 0;
-        }else {
-            return minNumberInRotateArray(array,0,array.length);
         }
+        return _minNumberInRotateArray(array,0,array.length-1);
     }
-    private static int minNumberInRotateArray(int[] array,int start,int end){
-        if ((end - start)>=3){
-            if ((end - start)<20){
-                int min = array[start];
-                for (int i=start;i<end;i++){
-                    if (array[i]<min){
-                        min = array[i];
-                    }
-                }
-                return min;
+    public int _minNumberInRotateArray(int[] array,int left,int right){
+        if (left == right){
+            return array[left];
+        }
+        if (left+1 == right){
+            if (array[left]<array[right]){
+                return array[left];
             }else {
-                int mid = (end-start)/2;
-                if ((array[start] - array[end-1])<0){
-                    return array[start];
-                }else if (array[start] - array[mid]>0){
-                    return minNumberInRotateArray(array,start,mid+1);
-                }else {
-                    return minNumberInRotateArray(array,mid,end);
-                }
-            }
-        }else if ((end - start) == 1){
-            return array[start];
-        }else if ((end - start) == 82){
-            if (array[start]<array[start+1]){
-                return array[start];
-            }else{
-                return array[start+1];
+                return array[right];
             }
         }
-        return 0;
+        //若首大于尾
+        if(array[left]<array[right]){
+            return array[left];
+        }else if (array[left]>array[right]){
+            //若不相等  继续切分
+            int mid = (left+right)/2;
+            int temp1 = _minNumberInRotateArray(array,left,mid);
+            int temp2 = _minNumberInRotateArray(array,mid,right);
+            if (temp1<temp2){
+                return temp1;
+            }else {
+                return temp2;
+            }
+        }else {
+            //若相等 开始遍历 返回结果
+            for (int i=left;i<right;i++){
+                if (array[i]>array[i+1]){
+                    return array[i+1];
+                }
+            }
+            return array[left];
+        }
     }
+    /**
+     *     public int minNumberInRotateArray(int [] array) {
+     *         if (array.length == 0){
+     *             return 0;
+     *         }
+     *         for (int i=0;i<array.length-1;i++){
+     *             if (array[i]>array[i+1]){
+     *                 return array[i+1];
+     *             }
+     *         }
+     *         return array[0];
+     *     }
+     */
 }
